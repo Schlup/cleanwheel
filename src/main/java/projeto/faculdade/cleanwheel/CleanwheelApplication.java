c98b0cb07bd1433a3bd51a4ada1326d884e5aca5
Schlup <alexschlup25@gmail.com>
2024-10-08 16:20:48 -0300

    Mensagem descrevendo o que foi feito

---
+++ src/main/java/projeto/faculdade/cleanwheel/CleanwheelApplication.java
@@ -0,0 +1,13 @@
+package projeto.faculdade.cleanwheel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanwheelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanwheelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projeto/faculdade/cleanwheel/CleanwheelApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.faculdade.cleanwheel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanwheelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
