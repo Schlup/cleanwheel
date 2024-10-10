96a160f8b4b84c2addeb010b220fa49114dc484c
Schlup <alexschlup25@gmail.com>
2024-10-10 00:55:09 -0300

    FIX: Postgres and Flyway Dockers are working properly now

--- db/migrations/V001__CREATE_PERSON_TABLE.sql
+++ db/migrations/V001__CREATE_PERSON_TABLE.sql
@@ -3,3 +3,9 @@ CREATE TABLE person (
     first_name varchar(50),
     last_name varchar(50)
 );
+
+CREATE TABLE person2 (
+    id serial PRIMARY KEY,
+    first_name varchar(50),
+    last_name varchar(50)
+);
